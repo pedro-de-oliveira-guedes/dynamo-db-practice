83018e33749e1cdfdd19b2a1b292b181985c96c3
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-01-08 10:54:23 -0300

    Feat: Creating a route to list the tables.

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
+var AWS = require('aws-sdk');
+
+const {
+    DynamoDB
+} = require('@aws-sdk/client-dynamodb');
+
 app.use(express.static(path.join(__dirname, '../frontend/styles')));
 
-// Route to serve the HTML page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/pages', 'index.html'));
+});
+
+app.get('/test', (req, res) => {
+    AWS.config.update({region: 'us-east-1'});
+
+    var dynamodb = new DynamoDB({
+        region: 'us-east-1'
+    });
+
+    var params = {}
+    dynamodb.listTables(params, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(data);
+        }
+    });
 });
 
 // Start the server
